f35f85dd4e79251ee1925f3a9b7275818db551ec
Andrew Mitty <cy.andrewmitty@gmail.com>
2018-05-04 12:14:40 +0300

    init project. change default controller to SController

---
+++ controllers/SController.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: amitty
+ * Date: 5/4/2018
+ * Time: 12:00 PM
+ */
---
+++ views/s/index.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: amitty
+ * Date: 5/4/2018
+ * Time: 11:43 AM
+ */
